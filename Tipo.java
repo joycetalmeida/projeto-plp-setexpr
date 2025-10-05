65f472b14cdbe558e82e97b23baf0593f8027cef
Joyce Tavares <90069817+joycetalmeida@users.noreply.github.com>
2025-10-05 18:59:46 -0300

    Update Tipo.java

--- Tipo.java
+++ Tipo.java
@@ -1,4 +1,4 @@
-// Interface que representa um tipo da nossa linguagem.
+// Interface que representa um tipo da linguagem.
 public interface Tipo {
     public String getNome();
     public boolean eIgual(Tipo outro);
