8faace6a7f39da0cfca13bd6236a5b80a0e95fa6
Joyce Tavares <90069817+joycetalmeida@users.noreply.github.com>
2025-10-05 18:57:53 -0300

    Update AmbienteExecucao.java

--- AmbienteExecucao.java
+++ AmbienteExecucao.java
@@ -1,4 +1,4 @@
 // Interface que representa o ambiente durante a execução do programa.
-// Guarda o valor das variáveis. Por enquanto, pode ser vazia.
+// Guarda o valor das variáveis.
 public interface AmbienteExecucao {
 }
